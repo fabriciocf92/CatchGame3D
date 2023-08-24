58894f5163284d79a53b5fc3ae8f4db34ca09ace
fabriciocf92 <fabricio.algol@gmail.com>
2023-08-24 04:34:52 -0300

    Added coin

--- Assets/Tests/PlayMode/Movements.cs
+++ Assets/Tests/PlayMode/Movements.cs
@@ -1,11 +1,9 @@
 using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 using UnityEngine.InputSystem;
-using System;
 
 public class Movements : InputTestFixture
 {
