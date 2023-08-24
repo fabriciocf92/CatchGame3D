e81319f9d828225077bf762a2ea300f1d5a878f9
fabriciocf92 <fabricio.algol@gmail.com>
2023-08-24 13:10:25 -0300

    Updating player1 score.

--- Assets/Scripts/PlayerScore.cs
+++ Assets/Scripts/PlayerScore.cs
@@ -1,17 +1,22 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScore : MonoBehaviour
 {
 
-    public int score;
+    private int Score;
+    public Text ScoreText;
+    private float LastCoinTime = 0;
 
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        if (hit.gameObject.CompareTag("Coin"))
+        if (hit.gameObject.CompareTag("Coin") && Time.timeSinceLevelLoad - LastCoinTime > 0.1)
         {
+            LastCoinTime = Time.timeSinceLevelLoad;
             var coin = hit.gameObject.GetComponent<Coin>();
-            score += coin.Value;
+            Score += coin.Value;
+            ScoreText.text = Score.ToString();
             coin.Spawn();
         }
     }
