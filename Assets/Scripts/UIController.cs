20ecbd9c19aa2665b134b53ba3465eb5fb9dbebe
fabriciocf92 <fabricio.algol@gmail.com>
2023-08-24 22:25:49 -0300

    Added pause option

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -6,10 +6,24 @@ using UnityEngine.SceneManagement;
 public class UIController : MonoBehaviour
 {
 
+    public GameObject paused;
+
     public void StartSingleplayer()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("Main");
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        paused.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        paused.SetActive(false);
     }
 
 }
